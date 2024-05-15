498656f157acec70481816c75d4bf5cca6ed97df
Marcos <exegeses@gmail.com>
2024-05-14 22:03:19 -0300

    Panel de administración de marcas

--- catalogo/routes/web.php
+++ catalogo/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MarcaController;
 
-Route::get('/', function () {
+//Route::get('peticion', acción);
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::view('/', 'plantilla');
+
+##################
+### crud de marcas
+Route::get('/marcas', [ MarcaController::class, 'index' ]);
